78a7b127cd78aa9491f3e7558f01a0642658180e
Heartwin Haveluck <heartwinhaveluck@gmail.com>
2024-06-22 18:30:44 +0100

    Removed boilerplate and added placeholder site

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,108 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
+<script setup lang="ts"></script>
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="wrapper">
+    <div class="textContainer">
+      <span class="mainText">Hey!</span>
+      <span class="subText1">You seem to be a lil'<br />too early for the party!</span>
+      <span class="subText2">I'm still building this thing!</span>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="imgContainer">
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&display=swap');
+html,
+body, #app {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+  background-color: #202020;
+  font-family: 'Montserrat', sans-serif;
+  color: white;
+  overflow: hidden;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.wrapper {
+  display: flex;
+  height: 100%;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+.textContainer {
+  display: flex;
+  flex-direction: column;
+  padding: 8rem;
+  height: calc(100% - 16rem);
+}
+.mainText {
+  font-size: 6rem;
+}
+.subText1 {
+  font-size: 3rem;
+  font-weight: 200;
+  margin-top: 2rem;
+}
+.subText2 {
+  margin-top: auto;
+  font-weight: 200;
+  font-size: 1.5rem;
+}
+.imgContainer {
+  position: relative;
+  flex-grow: 1;
+}
+.box {
+  position: absolute;
+  bottom: -100%;
+  background-color: #ffffffaa;
+}
+.box:nth-child(1) {
+  right: 10%;
+  width: 100px;
+  height: 100px;
+  border-radius: 12px;
+  animation: boxFloat 20s ease-out infinite;
+}
+.box:nth-child(2) {
+  right: 40%;
+  width: 200px;
+  height: 200px;
+  border-radius: 16px;
+  animation: boxFloat 40s 6s ease-out infinite;
+}
+.box:nth-child(3) {
+  right: 20%;
+  width: 50px;
+  height: 50px;
+  border-radius: 8px;
+  animation: boxFloat 15s 9s ease-out infinite;
+}
+.box:nth-child(4) {
+  right: 60%;
+  width: 100px;
+  height: 100px;
+  border-radius: 12px;
+  animation: boxFloat 25s 3s ease-out infinite;
+}
+.box:nth-child(5) {
+  right: 30%;
+  width: 150px;
+  height: 150px;
+  border-radius: 15px;
+  animation: boxFloat 28s 12s ease-out infinite;
+}
+@keyframes boxFloat {
+  from {
+    bottom: -10%;
+    transform: rotate(0deg);
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  to {
+    bottom: 110%;
+    transform: rotate(360deg);
   }
 }
 </style>
